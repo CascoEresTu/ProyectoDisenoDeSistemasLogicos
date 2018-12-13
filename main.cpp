426e6aab9df23b6b475c887c451239d7df722d84
CascoEresTu <cgcascovalle@unitec.edu>
2018-12-13 11:26:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,37 +31,44 @@ void loop()
     if ((unsigned long)(currentMillis - previousMillis1) >= interval1) // reiniciar el conteo
     {
         unseg = unseg + 1;
-
+        
+        //se hace reset de las unidades, y se agrega a la decena
         if (unseg == 10)
         {
             unseg = 0;
-            decseg = decseg + 1;
+            //decseg = decseg + 1;
+            decseg++;
         }
-
+        //se limpia la decena, y se agrega minuto    
         if (decseg == 6)
         {
             decseg = 0;
-            unmin = unmin + 1;
+            //unmin = unmin + 1;
+            unmin++;
         }
-
+         //se hace reset a la unidad de minuto, y se incrementa la decena 
         if (unmin == 10)
         {
             unmin = 0;
-            decmin = decmin + 1;
+            //decmin = decmin + 1;
+            decmin++;
         }
-
+        //.....
         if (decmin == 6)
         {
             decmin = 0;
-            horas = horas + 1;
+            //horas = horas + 1;
+            horas++;
         }
-
+        
+        //.....
         if (horas == 10)
         {
             horas = 0;
-            dechoras = dechoras + 1;
+            //dechoras = dechoras + 1;
+            dechoras++;
         }
-
+        //.....
         if (dechoras == 2)
         {
             dechoras = 0;
